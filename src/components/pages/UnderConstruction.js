66fab274442159ea7e6be272d03fc601ec6ffccd
unknown <elliot.redhead@gmail.com>
2020-05-14 21:40:01 +0100

    Created basic under construction page.

---
+++ src/components/pages/UnderConstruction.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UnderConstruction = () =>
+    <div>
+        <h1>This portfolio site is currently under construction.</h1>
+        <p>This is only temporary, but in the meantime: you can review my existing projects on my <a href="https://github.com/ElliotRedhead">GitHub profile</a>.</p>
+    </div>;
+
+export default UnderConstruction;
