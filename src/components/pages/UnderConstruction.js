1dcf312a0a53ee07257ae7da2cdee235b7a9aa32
unknown <elliot.redhead@gmail.com>
2020-07-23 21:17:41 +0100

    Added Google Analytics script.

--- src/components/pages/UnderConstruction.js
+++ src/components/pages/UnderConstruction.js
@@ -1,9 +1,16 @@
 import React from "react";
+import ReactGA from "react-ga";
 
-const UnderConstruction = () =>
+
+const UnderConstruction = () => {
+  ReactGA.initialize("G-2SBBFGDL16");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+  return (
     <div>
-        <h1>This portfolio site is currently under construction.</h1>
-        <p>This is only temporary, but in the meantime: you can review my existing projects on my <a href="https://github.com/ElliotRedhead">GitHub profile</a>.</p>
-    </div>;
+      <h1>This portfolio site is currently under construction.</h1>
+      <p>This is only temporary, but in the meantime: you can review my existing projects on my <a href="https://github.com/ElliotRedhead">GitHub profile</a>.</p>
+    </div>
+  );
+};
 
 export default UnderConstruction;
