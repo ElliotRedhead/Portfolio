700ecb5e9cd32e68a22d614144f8e57438de1b31
ElliotRedhead <elliot.redhead@gmail.com>
2020-08-15 21:59:26 +0100

    Added project details data.

---
+++ src/ProjectDetails.js
@@ -0,0 +1,20 @@
+const projectDetails = [
+  {
+    name: "PrintCrate",
+    shortDesc: "A Django-based ecommerce site for 3D-printed models.",
+    fullDesc: "",
+    technologies: ["HTML","CSS","Javascript","Python","Django","PostgreSQL","jQuery", "Amazon Web Services (AWS)","Heroku","Git"],
+    repoLink: "https://github.com/ElliotRedhead/PrintCrate",
+    liveLink:"https://printcrate.herokuapp.com/"
+  },
+  {
+    name: "Workout Planner",
+    shortDesc: "Customisable exercise planner, built with Flask.",
+    fullDesc: "",
+    technologies: ["HTML","CSS","Javascript","Python","Flask","MongoDB","jQuery", "Amazon Web Services (AWS)","Heroku","Git"],
+    repoLink: "https://github.com/ElliotRedhead/WorkoutPlanner",
+    liveLink:"https://workout-exercise-planner.herokuapp.com/welcome"
+  }
+];
+
+export default projectDetails;
