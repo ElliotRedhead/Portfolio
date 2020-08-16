912eba0ee46983a3682174c6c0bd0aceaec2428e
ElliotRedhead <elliot.redhead@gmail.com>
2020-08-16 15:05:40 +0100

    Corrected import  for Link. (react-router-dom).

--- src/components/HiddenLink.js
+++ src/components/HiddenLink.js
@@ -1,4 +1,5 @@
-import React, {useState, Link} from "react";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
 const HiddenLink = () => {
   const [linkVisibility, switchLinkVisibility] = useState("hidden");
