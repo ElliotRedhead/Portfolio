ee8d9af09ca32eb2cb6ce01bb4c66861215b38a9
ElliotRedhead <elliot.redhead@gmail.com>
2020-08-16 22:44:33 +0100

    Corrected cursor interval & offset bugs.

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -12,30 +12,23 @@ const Homepage = () => {
 
   const [cursorDisplay, setCursorDisplay] = useState("");
 
-  let blinkPreviouslyFired = false;
   const titleHeading = useRef(null);
-  // const titleHeading = useRef(blinkPreviouslyFired);
-
-  const titleCursorBlink = () => {
-    setInterval(() => {
-      console.log("Blink fired");
-      cursorDisplay === "" ? setCursorDisplay("_") : setCursorDisplay("");
-    }, 500);
-  };
-
 
   useEffect(() => {
-    if(titleHeading.current.innerHTML.startsWith(titleText) && !blinkPreviouslyFired){
-      // blinkPreviouslyFired = true;
-      titleCursorBlink();
+    if(titleHeading.current.innerHTML.startsWith(titleText)){
+
+      const interval = setInterval(() => {
+        cursorDisplay === "" ? setCursorDisplay("_") : setCursorDisplay("");
+      }, 700);
+      return () => clearInterval(interval);
     }
-  });
+  }, [headingText, cursorDisplay]);
 
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="jumbotron col-12 mx-auto text-center">
-          <h1 id="titleHeading" ref={titleHeading}>{headingText}<span>{cursorDisplay}</span></h1>
+        <div className="jumbotron col-12">
+          <h1 id="titleHeading" ref={titleHeading} className="text-center">{headingText}<span id="titleCursor">{cursorDisplay}</span></h1>
           <Link to="404" className="">
         Learn More
           </Link>
