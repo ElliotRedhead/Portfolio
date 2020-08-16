116947c088d58bf0c149d54a70c4b33323cd4c08
ElliotRedhead <elliot.redhead@gmail.com>
2020-08-16 02:15:58 +0100

    Made project preview container.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import pageNotFound404 from "./pages/PageNotFound404";
-import NHLTeamShowdown from "./modals/NHLTeamShowdown";
 import UnderConstruction from "./pages/UnderConstruction";
 
 const App = () => (
@@ -10,7 +9,6 @@ const App = () => (
     <Switch>
       <Route exact path={["/","/ReactPortfolio/"]} component={UnderConstruction} />
       <Route exact path={["/home"]} component={Homepage} />
-      <Route path={["/NHLTeamShowdown"]} component={NHLTeamShowdown} />
       <Route component={pageNotFound404} />
     </Switch>
   </div>
