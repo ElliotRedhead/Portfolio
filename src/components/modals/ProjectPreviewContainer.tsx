080f5fbc39df56de3ef51581c2769041ca023083
ElliotRedhead <elliot.redhead@gmail.com>
2020-08-17 22:13:43 +0100

    Converted project preview container to Typescript.

---
+++ src/components/modals/ProjectPreviewContainer.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { VisibilityProperty } from "csstype"; // eslint-disable-line no-unused-vars
+
+/**
+ * React Hooks are used to toggle visibility of this element, a conditional ternary operator determines what visibility to pass based on the current state.
+ * @returns {object} The project preview object.
+ */
+const ProjectPreviewContainer=() => {
+  const [containerVisibility, switchComponentVisibility] = useState<VisibilityProperty>("hidden");
+
+  return (
+    <div
+      onMouseOver={() => {
+        switchComponentVisibility("visible");
+      }}
+      onMouseOut={() => {
+        switchComponentVisibility("hidden");
+      }}
+    >
+      <button style={{ visibility: containerVisibility }}>
+        Only visible on parent element hover.
+      </button>
+    </div>
+  );
+};
+
+export default ProjectPreviewContainer;
