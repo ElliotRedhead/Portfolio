d1d42b57689e9fbb379079c6c11725c476564ef7
ElliotRedhead <elliot.redhead@gmail.com>
2020-08-18 23:17:55 +0100

    Altered imports to reflect new file structure.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import pageNotFound404 from "./pages/PageNotFound404";
-import UnderConstruction from "./pages/UnderConstruction";
+import Homepage from "../pages/Homepage";
+import pageNotFound404 from "../pages/PageNotFound404";
+import UnderConstruction from "../pages/UnderConstruction";
 
 const App = () => (
   <div>
