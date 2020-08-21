c45301db35f0a4b370097c5725cb960864fb2557
ElliotRedhead <elliot.redhead@gmail.com>
2020-08-21 08:14:14 +0100

    Added portfolio project data, and Travis CI note.

--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -1,9 +1,17 @@
 const projectDetails = [
   {
+    name: "React Portfolio",
+    shortDesc: "An interactive project showcase.",
+    fullDesc: "",
+    technologies: ["HTML","SASS","Javascript","React", "React Hooks", "Git"],
+    repoLink: "https://github.com/ElliotRedhead/ReactPortfolio",
+    liveLink:"https://www.elliotredhead.com/"
+  },
+  {
     name: "PrintCrate",
     shortDesc: "A Django-based ecommerce site for 3D-printed models.",
     fullDesc: "",
-    technologies: ["HTML","CSS","Javascript","Python","Django","PostgreSQL","jQuery", "Amazon Web Services (AWS)","Heroku","Git"],
+    technologies: ["HTML","CSS","Javascript","Python","Django","PostgreSQL","jQuery", "Amazon Web Services (AWS)","Travis CI", "Heroku","Git"],
     repoLink: "https://github.com/ElliotRedhead/PrintCrate",
     liveLink:"https://printcrate.herokuapp.com/"
   },
@@ -11,7 +19,7 @@ const projectDetails = [
     name: "Workout Planner",
     shortDesc: "Customisable exercise planner, built with Flask.",
     fullDesc: "",
-    technologies: ["HTML","CSS","Javascript","Python","Flask","MongoDB","jQuery", "Amazon Web Services (AWS)","Heroku","Git"],
+    technologies: ["HTML","CSS","Javascript","Python","Flask","MongoDB","jQuery", "Amazon Web Services (AWS)","Travis CI","Heroku","Git"],
     repoLink: "https://github.com/ElliotRedhead/WorkoutPlanner",
     liveLink:"https://workout-exercise-planner.herokuapp.com/welcome"
   }
