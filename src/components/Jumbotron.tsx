7444bd40b1424e7de455b8b4becbd8a7dfe393ab
Elliot Redhead <elliot.redhead@gmail.com>
2020-08-23 21:29:01 +0100

    Extracted jumbotron component.

---
+++ src/components/Jumbotron.tsx
@@ -0,0 +1,56 @@
+import React, { useLayoutEffect, useRef } from "react";
+import "./../styles/jumbotron.scss";
+import useTypewriter from "react-typewriter-hook";
+import { useState } from "react";
+
+const Jumbotron = () => {
+  const titleText = "Elliot Redhead";
+  const animatedHeading = useTypewriter(titleText);
+
+  const [cursorDisplay, setCursorDisplay] = useState("_");
+
+  const titleReference = useRef<HTMLDivElement>(null);
+  let additionalSubtitleClasses = useRef("invisible");
+
+
+
+  useLayoutEffect(() => {
+    const titleNode = titleReference.current;
+    additionalSubtitleClasses.current = "invisible";
+
+
+    if (titleNode && titleNode.innerHTML.startsWith(titleText)) {
+      additionalSubtitleClasses.current = "fadeIn";
+      const interval = setInterval(() => {
+        cursorDisplay === "" ? setCursorDisplay("_") : setCursorDisplay("");
+      }, 700);
+      return () => clearInterval(interval);
+    }
+
+  }, [animatedHeading, cursorDisplay]);
+
+
+  return (
+    <div className="jumbotron d-flex align-items-center">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <h1 id="titleHeading" ref={titleReference} className="text-center mb-3">
+              {animatedHeading}
+              <span id="titleCursor">{cursorDisplay}</span>
+            </h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <h2 id="subHeading" className={`text-center ${additionalSubtitleClasses.current}`}>
+          Web Developer
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,57 +1,8 @@
-import React, { useLayoutEffect, useRef } from "react";
-import "../styles/global.scss";
-import useTypewriter from "react-typewriter-hook";
-import { useState } from "react";
-// import detectBreakpoint from "../utilities/DetectBreakpoint";
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
 
-const Homepage = () => {
-  const titleText = "Elliot Redhead";
-  const animatedHeading = useTypewriter(titleText);
-
-  const [cursorDisplay, setCursorDisplay] = useState("_");
-
-  const titleReference = useRef<HTMLDivElement>(null);
-  let additionalSubtitleClasses = useRef("invisible");
-
-
-
-  useLayoutEffect(() => {
-    const titleNode = titleReference.current;
-    additionalSubtitleClasses.current = "invisible";
-
-
-    if (titleNode && titleNode.innerHTML.startsWith(titleText)) {
-      additionalSubtitleClasses.current = "fadeIn";
-      const interval = setInterval(() => {
-        cursorDisplay === "" ? setCursorDisplay("_") : setCursorDisplay("");
-      }, 700);
-      return () => clearInterval(interval);
-    }
-
-  }, [animatedHeading, cursorDisplay]);
-
-
-  return (
-    <div className="jumbotron d-flex align-items-center">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <h1 id="titleHeading" ref={titleReference} className="text-center mb-3">
-              {animatedHeading}
-              <span id="titleCursor">{cursorDisplay}</span>
-            </h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <h2 id="subHeading" className={`text-center ${additionalSubtitleClasses.current}`}>
-          Web Developer
-            </h2>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+const Homepage = () => (
+  <Jumbotron />
+);
 
 export default Homepage;
