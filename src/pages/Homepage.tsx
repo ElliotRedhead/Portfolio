ffd587b7babe2e164d2d7514d09771c2c5d7bf6d
ElliotRedhead <elliot.redhead@gmail.com>
2020-08-23 17:55:38 +0100

    Added subtitle following title animation.

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,33 +2,52 @@ import React, { useLayoutEffect, useRef } from "react";
 import "../styles/global.scss";
 import useTypewriter from "react-typewriter-hook";
 import { useState } from "react";
+// import detectBreakpoint from "../utilities/DetectBreakpoint";
 
 const Homepage = () => {
-  const titleText = "Elliot Redhead | Web Developer";
-  const headingText = useTypewriter(titleText);
+  const titleText = "Elliot Redhead";
+  const animatedHeading = useTypewriter(titleText);
 
   const [cursorDisplay, setCursorDisplay] = useState("");
 
-  const titleHeading = useRef<HTMLDivElement>(null);
+  const titleReference = useRef<HTMLDivElement>(null);
+  let additionalSubtitleClasses = useRef("invisible");
+
+
 
   useLayoutEffect(() => {
-    const titleNode = titleHeading.current;
+    const titleNode = titleReference.current;
+    additionalSubtitleClasses.current = "invisible";
+
+
     if (titleNode && titleNode.innerHTML.startsWith(titleText)) {
+      additionalSubtitleClasses.current = "fadeIn";
       const interval = setInterval(() => {
         cursorDisplay === "" ? setCursorDisplay("_") : setCursorDisplay("");
       }, 700);
       return () => clearInterval(interval);
     }
-  }, [headingText, cursorDisplay]);
+
+  }, [animatedHeading, cursorDisplay]);
+
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="jumbotron-fluid col-10 offset-1 col-md-12 offset-md-0 d-flex justify-content-center align-items-center">
-          <h1 id="titleHeading" ref={titleHeading} className="text-center">
-            {headingText}
-            <span id="titleCursor">{cursorDisplay}</span>
-          </h1>
+    <div className="jumbotron d-flex align-items-center">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <h1 id="titleHeading" ref={titleReference} className="text-center mb-3">
+              {animatedHeading}
+              <span id="titleCursor">{cursorDisplay}</span>
+            </h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <h2 id="subHeading" className={`text-center ${additionalSubtitleClasses.current}`}>
+          Web Developer
+            </h2>
+          </div>
         </div>
       </div>
     </div>
