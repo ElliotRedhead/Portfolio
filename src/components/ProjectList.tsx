56c5c8d678f8f0b0ce05d8bf73452acc3fe25cd0
ElliotRedhead <elliot.redhead@gmail.com>
2020-09-07 21:48:59 +0100

    Corrected error, ref not part of div.

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -7,8 +7,8 @@ const ProjectList = () => {
   return (
     <div
       id="projectlist"
-      className="container-fluid">
-          ref={projectlist}
+      className="container-fluid"
+      ref={projectlist}>
       <p>test</p>
     </div>
   );
