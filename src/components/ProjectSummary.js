626fa85b09fb733066238d3919441493ed79e618
elliot.redhead <elliot.redhead@gmail.com>
2020-09-20 23:28:25 +0100

    Iteration to display tech icons.

--- src/components/ProjectSummary.js
+++ src/components/ProjectSummary.js
@@ -1,16 +1,25 @@
 import React from "react";
 
-const ProjectSummary = ({name, shortDesc, technologies, repoLink, liveLink}) => (
-  <>
-    <div className="col-12 col-md-4">
-      <p>{name}</p>
-      <p>{shortDesc}</p>
-      <p>{technologies}</p>
-      <p><a target="_blank" rel="noopener noreferrer" href={repoLink}>Repo Link</a></p>
-      <p><a target="_blank" rel="noopener noreferrer" href={liveLink}>Live Link</a></p>
-    </div>
-  </>
-);
+const ProjectSummary = ({ name, shortDesc, technologies, repoLink, liveLink }) => {
+  const technologyIcons = technologies.map((technology) => {
+    const techImageSource = `/images/techicons/${technology.toLowerCase()}.png`;
+    return <img src={techImageSource} />;
+  });
+
+  return (
+    <>
+      <div className="col-12 col-md-4">
+        <div className="d-flex flex-column justify-content-around">
+          <p>{name}</p>
+          <p>{shortDesc}</p>
+          {technologyIcons}
+          <button><a target="_blank" rel="noopener noreferrer" href={repoLink}>Repo Link</a></button>
+          <button><a target="_blank" rel="noopener noreferrer" href={liveLink}>Live Link</a></button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 
 export default ProjectSummary;
