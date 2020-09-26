a5d72cf3e77c9cf54e74a00d33dd43e535b814b6
ElliotRedhead <elliot.redhead@gmail.com>
2020-09-26 22:45:13 +0100

    Removed redundant package use.

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,5 +1,4 @@
 import React, { useRef } from "react";
-import Div100vh from "react-div-100vh";
 import "./../styles/projectlist.scss";
 import projectData from "../ProjectData";
 import ProjectSummary from "./ProjectSummary";
@@ -10,29 +9,27 @@ const ProjectList = () => {
   //   console.log(projectData.length); Can be used with breakpoint to determine total number of projects.
 
   return (
-    <Div100vh>
-      <div
-        id="projectList"
-        className="container-fluid d-flex flex-column justify-content-center"
-        ref={projectlist}>
-        <div className="row">
-          {projectData.map(
-            ({ id, name, shortDesc, technologies, repoLink, liveLink }) => {
-              return(
-                <ProjectSummary
-                  key={id}
-                  name={name}
-                  shortDesc={shortDesc}
-                  technologies={technologies}
-                  repoLink={repoLink}
-                  liveLink={liveLink}
-                />
-              );
-            }
-          )}
-        </div>
+    <div
+      id="projectList"
+      className="container-fluid d-flex flex-column justify-content-center"
+      ref={projectlist}>
+      <div className="row">
+        {projectData.map(
+          ({ id, name, shortDesc, technologies, repoLink, liveLink }) => {
+            return(
+              <ProjectSummary
+                key={id}
+                name={name}
+                shortDesc={shortDesc}
+                technologies={technologies}
+                repoLink={repoLink}
+                liveLink={liveLink}
+              />
+            );
+          }
+        )}
       </div>
-    </Div100vh>
+    </div>
   );
 };
 
