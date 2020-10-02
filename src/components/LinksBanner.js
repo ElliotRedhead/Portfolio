96bf32e82df9ca1b1e4f4d36f607426505298128
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-02 17:45:55 +0100

    Banner links styling & icon choice.

--- src/components/LinksBanner.js
+++ src/components/LinksBanner.js
@@ -3,8 +3,8 @@ import React from "react";
 const LinksBanner = () => {
   return(
     <>
-      <img src="/images/linkicons/github-square.svg" alt="GitHub link icon." style={{position: "absolute", right:"5rem", width:"50px", height: "70px", objectFit: "cover"}}/>
-      <img src="/images/linkicons/cv-doc.svg" alt="CV download link icon" style={{position: "absolute", right:"10rem", width:"50px", height: "70px", objectFit:"cover"}}/>
+      <img src="/images/linkicons/github-brands.svg" alt="GitHub link icon." style={{position: "absolute", top:"1rem", right:"6rem", width:"2.5rem", height: "2.5rem", objectFit: "contain"}}/>
+      <img src="/images/linkicons/cv-doc.svg" alt="CV download link icon" style={{position: "absolute", top:"1rem", right:"2rem", width:"2.5rem", height: "2.5rem", objectFit:"contain"}}/>
     </>
   );
 };
