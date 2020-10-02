9c5d94684e809d6e9dacdaa2e9c992f7eb6a64d9
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-02 18:21:19 +0100

    Added links to github and file download.

--- src/components/LinksBanner.js
+++ src/components/LinksBanner.js
@@ -3,8 +3,12 @@ import React from "react";
 const LinksBanner = () => {
   return(
     <>
-      <img src="/images/linkicons/github-brands.svg" alt="GitHub link icon." style={{position: "absolute", top:"1rem", right:"6rem", width:"2.5rem", height: "2.5rem", objectFit: "contain"}}/>
-      <img src="/images/linkicons/cv-doc.svg" alt="CV download link icon" style={{position: "absolute", top:"1rem", right:"2rem", width:"2.5rem", height: "2.5rem", objectFit:"contain"}}/>
+      <a href="http://www.github.com/elliotredhead" target="_blank" rel="noreferrer">
+        <img src="/images/linkicons/github-brands.svg" alt="GitHub link icon." style={{position: "absolute", top:"1rem", right:"6rem", width:"2.5rem", height: "2.5rem", objectFit: "contain"}}/>
+      </a>
+      <a href="https://elliot-portfolio.s3.eu-west-2.amazonaws.com/Elliot_Redhead_CV.pdf" target="_blank" rel="noreferrer">
+        <img src="/images/linkicons/cv-doc.svg" alt="CV download link icon" style={{position: "absolute", top:"1rem", right:"2rem", width:"2.5rem", height: "2.5rem", objectFit:"contain"}}/>
+      </a>
     </>
   );
 };
