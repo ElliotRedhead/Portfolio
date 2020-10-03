695ade9484d8b457ed9bfe3c616921352193d0be
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-03 16:56:14 +0100

    Added bootstrap skill to projects list.

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -4,7 +4,7 @@ const projectData = [
     name: "React Portfolio",
     shortDesc: "An interactive project showcase, displaying featured projects.",
     fullDesc: "",
-    technologies: ["HTML","SASS","Javascript","React","Amazon Web Services (AWS)", "Git"],
+    technologies: ["HTML","SASS","Javascript","React","Amazon Web Services (AWS)","Travis CI", "Git", "Bootstrap"],
     repoLink: "https://github.com/ElliotRedhead/ReactPortfolio",
     liveLink:"https://www.elliotredhead.com/"
   },
@@ -13,7 +13,7 @@ const projectData = [
     name: "PrintCrate",
     shortDesc: "A Django-based ecommerce site for 3D-printed models.",
     fullDesc: "",
-    technologies: ["HTML","CSS","Javascript","Python","Django","PostgreSQL","jQuery", "Amazon Web Services (AWS)","Travis CI", "Heroku","Git"],
+    technologies: ["HTML","CSS","Javascript","Python","Django","PostgreSQL","jQuery", "Amazon Web Services (AWS)","Travis CI", "Heroku","Git", "Bootstrap"],
     repoLink: "https://github.com/ElliotRedhead/PrintCrate",
     liveLink:"https://printcrate.herokuapp.com/"
   },
@@ -22,7 +22,7 @@ const projectData = [
     name: "Workout Planner",
     shortDesc: "Customisable exercise planner, built with Flask.",
     fullDesc: "",
-    technologies: ["HTML","CSS","Javascript","Python","Flask","MongoDB","jQuery", "Amazon Web Services (AWS)","Travis CI","Heroku","Git"],
+    technologies: ["HTML","CSS","Javascript","Python","Flask","MongoDB","jQuery", "Amazon Web Services (AWS)","Travis CI","Heroku","Git", "Bootstrap"],
     repoLink: "https://github.com/ElliotRedhead/WorkoutPlanner",
     liveLink:"https://workout-exercise-planner.herokuapp.com/welcome"
   }
