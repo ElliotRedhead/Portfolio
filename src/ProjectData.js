7f12775126c3b6c5f2077f53b38e7c1fee98f543
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-17 18:00:26 +0100

    Added GitStats project details.

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,6 +1,15 @@
 const projectData = [
   {
     id: 1,
+    name: "GitStats",
+    shortDesc: "WIP - GitHub commit statistics, drilldown on any public repo using React & REST API.",
+    fullDesc: "",
+    technologies: ["HTML","SASS","Javascript","React","Material UI", "Git"],
+    repoLink: "https://github.com/ElliotRedhead/GitStats",
+    liveLink: "https://elliotredhead.github.io/GitStats/"
+  },
+  {
+    id: 2,
     name: "React Portfolio",
     shortDesc: "An interactive project showcase, displaying featured projects.",
     fullDesc: "",
@@ -9,7 +18,7 @@ const projectData = [
     liveLink:"https://www.elliotredhead.com/"
   },
   {
-    id: 2,
+    id: 3,
     name: "PrintCrate",
     shortDesc: "A Django-based ecommerce site for 3D-printed models.",
     fullDesc: "",
@@ -18,7 +27,7 @@ const projectData = [
     liveLink:"https://printcrate.herokuapp.com/"
   },
   {
-    id: 3,
+    id: 4,
     name: "Workout Planner",
     shortDesc: "Customisable exercise planner, built with Flask.",
     fullDesc: "",
@@ -27,7 +36,7 @@ const projectData = [
     liveLink:"https://workout-exercise-planner.herokuapp.com/welcome"
   },
   {
-    id: 4,
+    id: 5,
     name: "NHL Showdown",
     shortDesc: "Statistics face-off between two NHL teams.",
     fullDesc: "",
@@ -36,7 +45,7 @@ const projectData = [
     liveLink:"https://elliotredhead.github.io/NHLTeamShowdown"
   },
   {
-    id: 5,
+    id: 6,
     name: "Introduction to ICPMS",
     shortDesc: "Beginner's intro to Inductively Coupled Plasma Mass Spectrometry",
     fullDesc: "",
