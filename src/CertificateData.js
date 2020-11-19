6914e444da53c085b3352bc2b968e36ad90fc23e
elliot.redhead <elliot.redhead@gmail.com>
2020-11-19 17:37:35 +0000

    Fix key indexes for certificates.

--- src/CertificateData.js
+++ src/CertificateData.js
@@ -1,39 +1,39 @@
 const certificateData = [
-  {
-    id: 1,
-    name: "Code Institute Full Stack Bootcamp",
-    shortDesc: "",
-    certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/Elliot-Redhead-Code-Institute-First-Class.pdf",
-  },
-  {
-    id: 3,
-    name: "React, The Big Picture",
-    shortDesc: "",
-    certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/react-the-big-picture.pdf",
-    proficiency:"React"
-  },
-  {
-    id: 2,
-    name: "React Fundamentals",
-    shortDesc: "",
-    certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/react-fundamentals.pdf",
-    proficiency: "React"
+	{
+		id: 1,
+		name: "Code Institute Full Stack Bootcamp",
+		shortDesc: "",
+		certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/Elliot-Redhead-Code-Institute-First-Class.pdf",
+	},
+	{
+		id: 2,
+		name: "React, The Big Picture",
+		shortDesc: "",
+		certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/react-the-big-picture.pdf",
+		proficiency:"React"
+	},
+	{
+		id: 3,
+		name: "React Fundamentals",
+		shortDesc: "",
+		certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/react-fundamentals.pdf",
+		proficiency: "React"
 
-  },
-  {
-    id: 3,
-    name: "Building Applications With React And Redux",
-    shortDesc: "",
-    certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/building-applications-with-react-and-redux.pdf",
-    proficiency:"React"
-  },
-  {
-    id: 4,
-    name: "Using React Hooks",
-    shortDesc: "",
-    certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/using-react-hooks.pdf",
-    proficiency: "React"
-  },
+	},
+	{
+		id: 4,
+		name: "Building Applications With React And Redux",
+		shortDesc: "",
+		certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/building-applications-with-react-and-redux.pdf",
+		proficiency:"React"
+	},
+	{
+		id: 5,
+		name: "Using React Hooks",
+		shortDesc: "",
+		certLink:"https://elliot-portfolio.s3.eu-west-2.amazonaws.com/using-react-hooks.pdf",
+		proficiency: "React"
+	},
 ];
 
 export default certificateData;
