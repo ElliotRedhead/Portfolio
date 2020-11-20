543bd2a230433a2d906eb610ffe8f2c5869ef9aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-20 19:49:15 +0000

    Deploy ElliotRedhead/ReactPortfolio to github.com/ElliotRedhead/ReactPortfolio.git:gh-pages

---
+++ precache-manifest.4ed850881c71b036223be1726b2ef383.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1749ad6eb29c963fc30d71a32181db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "046ff81da8621fee7bfc",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3d77752191f353fe6146",
+    "url": "/static/css/main.d9218867.chunk.css"
+  },
+  {
+    "revision": "046ff81da8621fee7bfc",
+    "url": "/static/js/2.4a640036.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4a640036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d77752191f353fe6146",
+    "url": "/static/js/main.50421da2.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
