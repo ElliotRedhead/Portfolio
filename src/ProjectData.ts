084804b4714bfdc22628c7dd39b0767e34d25139
ElliotRedhead <elliot.redhead@gmail.com>
2021-03-10 08:45:48 +0000

    Type check project data.

--- src/ProjectData.ts
+++ src/ProjectData.ts
@@ -1,10 +1,20 @@
-const projectData = [
+interface projectData {
+	id:number,
+	name:string,
+	shortDesc:string,
+	fullDesc:string,
+	technologies:Array<string>,
+	repoLink:string,
+	liveLink:string,
+}
+
+const projectData: projectData[] = [
 	{
 		id: 1,
 		name: "Your Inventory Manager",
 		shortDesc: "WIP - Visibility on inventory levels with user authorisation. Full stack project with Node, Express & React.",
 		fullDesc: "",
-		technologies: ["HTML","Javascript","React","Node", "Express", "PostgreSQL", "Git", "Bootstrap", "Yarn", "Docker"],
+		technologies: ["HTML", "Javascript", "React", "Node", "Express", "PostgreSQL", "Git", "Bootstrap", "Yarn", "Docker"],
 		repoLink: "https://github.com/ElliotRedhead/YourInventoryManager",
 		liveLink: "#"
 	},
@@ -13,7 +23,7 @@ const projectData = [
 		name: "GitStats",
 		shortDesc: "WIP - GitHub commit statistics, drilldown on any public repo using React & REST API.",
 		fullDesc: "",
-		technologies: ["HTML","SASS","Javascript","React","Material UI", "Travis CI", "Git", "Yarn"],
+		technologies: ["HTML", "SASS", "Javascript", "React", "Material UI", "Travis CI", "Git", "Yarn"],
 		repoLink: "https://github.com/ElliotRedhead/GitStats",
 		liveLink: "https://elliotredhead.github.io/GitStats/"
 	},
@@ -22,36 +32,36 @@ const projectData = [
 		name: "React Portfolio",
 		shortDesc: "An interactive project showcase, displaying featured projects.",
 		fullDesc: "",
-		technologies: ["HTML","SASS","Javascript","React","Amazon Web Services (AWS)","Travis CI", "Git", "Bootstrap", "Yarn"],
+		technologies: ["HTML", "SASS", "Javascript", "React", "Amazon Web Services (AWS)", "Travis CI", "Git", "Bootstrap", "Yarn"],
 		repoLink: "https://github.com/ElliotRedhead/ReactPortfolio",
-		liveLink:"https://www.elliotredhead.com/"
+		liveLink: "https://www.elliotredhead.com/"
 	},
 	{
 		id: 4,
 		name: "PrintCrate",
 		shortDesc: "A Django-based ecommerce site for 3D-printed models.",
 		fullDesc: "",
-		technologies: ["HTML","CSS","Javascript","Python","Django","PostgreSQL","jQuery", "Amazon Web Services (AWS)","Travis CI", "Heroku","Git", "Bootstrap"],
+		technologies: ["HTML", "CSS", "Javascript", "Python", "Django", "PostgreSQL", "jQuery", "Amazon Web Services (AWS)", "Travis CI", "Heroku", "Git", "Bootstrap"],
 		repoLink: "https://github.com/ElliotRedhead/PrintCrate",
-		liveLink:"https://printcrate.herokuapp.com/"
+		liveLink: "https://printcrate.herokuapp.com/"
 	},
 	{
 		id: 5,
 		name: "Workout Planner",
 		shortDesc: "Customisable exercise planner, built with Flask.",
 		fullDesc: "",
-		technologies: ["HTML","CSS","Javascript","Python","Flask","MongoDB","jQuery", "Amazon Web Services (AWS)","Travis CI","Heroku","Git", "Bootstrap"],
+		technologies: ["HTML", "CSS", "Javascript", "Python", "Flask", "MongoDB", "jQuery", "Amazon Web Services (AWS)", "Travis CI", "Heroku", "Git", "Bootstrap"],
 		repoLink: "https://github.com/ElliotRedhead/WorkoutPlanner",
-		liveLink:"https://workout-exercise-planner.herokuapp.com/welcome"
+		liveLink: "https://workout-exercise-planner.herokuapp.com/welcome"
 	},
 	{
 		id: 6,
 		name: "NHL Showdown",
 		shortDesc: "Statistics face-off between two NHL teams.",
 		fullDesc: "",
-		technologies: ["HTML","CSS","Javascript","jQuery","Git", "Bootstrap"],
+		technologies: ["HTML", "CSS", "Javascript", "jQuery", "Git", "Bootstrap"],
 		repoLink: "https://github.com/ElliotRedhead/NHLTeamShowdown",
-		liveLink:"https://elliotredhead.github.io/NHLTeamShowdown"
+		liveLink: "https://elliotredhead.github.io/NHLTeamShowdown"
 	}
 ];
 
